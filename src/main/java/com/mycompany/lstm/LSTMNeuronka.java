84ca712694d42a6aaa69f2c160e22195ff8f6863
Patrik <pacek99@gmail.com>
2019-04-15 11:27:21 +0200

    uprava evaluacie a pridanie testovacich dat

--- src/main/java/com/mycompany/lstm/LSTMNeuronka.java
+++ src/main/java/com/mycompany/lstm/LSTMNeuronka.java
@@ -40,6 +40,7 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
@@ -67,11 +68,12 @@ public class LSTMNeuronka {
     private static File featuresDirTest = new File(baseTestDir, "features");
     private static File labelsDirTest = new File(baseTestDir, "labels");
     
-    private static int filesCount;
+    private static int trainCount;
+    private static int testCount;
     
     //variables from Neuronka project
     private static Map<Double, Double> map;
-    private static String sensor = "AK09918C";
+    private static String sensor = "ST_LIS3DHTR";
     private static String processedDataFile = "processedData.csv";
     private static List<double[]> oneActivity;
     private static String csvSplitBy = "\t";
@@ -89,18 +91,20 @@ public class LSTMNeuronka {
         featuresDirTest.mkdir();
         labelsDirTest.mkdir();
         
-        filesCount = 0;
+        trainCount = 0;
+        testCount = 0;
         
         // TODO code application logic here
         Map<String, String> subory = new HashMap<String,String>();
+        Map<String, String> suboryNaTest = new HashMap<String,String>();
         
         // prieèinok B. Taylorová
-        subory.put("src/main/resources/B. Taylorová/indora-1549482603748.csv", "MPU6500 Acceleration Sensor");        
-        subory.put("src/main/resources/B. Taylorová/indora-1549482689471.csv", "MPU6500 Acceleration Sensor");        
-        subory.put("src/main/resources/B. Taylorová/indora-1549482748787.csv", "MPU6500 Acceleration Sensor");        
-        subory.put("src/main/resources/B. Taylorová/indora-1549482827151.csv", "MPU6500 Acceleration Sensor");        
-        subory.put("src/main/resources/B. Taylorová/indora-1549482928447.csv", "MPU6500 Acceleration Sensor");        
-        subory.put("src/main/resources/B. Taylorová/indora-1549482960030.csv", "MPU6500 Acceleration Sensor");
+        //subory.put("src/main/resources/B. Taylorová/indora-1549482603748.csv", "MPU6500 Acceleration Sensor");        
+        //subory.put("src/main/resources/B. Taylorová/indora-1549482689471.csv", "MPU6500 Acceleration Sensor");        
+        //subory.put("src/main/resources/B. Taylorová/indora-1549482748787.csv", "MPU6500 Acceleration Sensor");        
+        //subory.put("src/main/resources/B. Taylorová/indora-1549482827151.csv", "MPU6500 Acceleration Sensor");        
+        //subory.put("src/main/resources/B. Taylorová/indora-1549482928447.csv", "MPU6500 Acceleration Sensor");        
+        //subory.put("src/main/resources/B. Taylorová/indora-1549482960030.csv", "MPU6500 Acceleration Sensor");
                       
         //subory.put("src/main/resources/B. Taylorová/indora-1552906231896.csv", "MPU6500 Acceleration Sensor");       
         //subory.put("src/main/resources/B. Taylorová/indora-1552906271137.csv", "MPU6500 Acceleration Sensor");       
@@ -113,15 +117,15 @@ public class LSTMNeuronka {
         //subory.put("src/main/resources/B. Taylorová/indora-1553278160954.csv", "MPU6500 Acceleration Sensor");  
         
         // prieèinok M. Sochuliak
-        subory.put("src/main/resources/M. Sochuliak/indora-1549012172677.csv", "ACCELEROMETER");        
-        subory.put("src/main/resources/M. Sochuliak/indora-1549021777198.csv", "ACCELEROMETER");        
-        subory.put("src/main/resources/M. Sochuliak/indora-1549022025135.csv", "ACCELEROMETER");        
-        subory.put("src/main/resources/M. Sochuliak/indora-1549022068213.csv", "ACCELEROMETER");
+        //subory.put("src/main/resources/M. Sochuliak/indora-1549012172677.csv", "ACCELEROMETER");        
+        //subory.put("src/main/resources/M. Sochuliak/indora-1549021777198.csv", "ACCELEROMETER");        
+        //subory.put("src/main/resources/M. Sochuliak/indora-1549022025135.csv", "ACCELEROMETER");        
+        //subory.put("src/main/resources/M. Sochuliak/indora-1549022068213.csv", "ACCELEROMETER");
         
         // prieèinok P. Kendra
-        subory.put("src/main/resources/P. Kendra/indora-1549541475108.csv", sensor);        
+        suboryNaTest.put("src/main/resources/P. Kendra/indora-1549541475108.csv", sensor);        
         subory.put("src/main/resources/P. Kendra/indora-1549541522778.csv", sensor);        
-        subory.put("src/main/resources/P. Kendra/indora-1549541559653.csv", sensor);        
+        suboryNaTest.put("src/main/resources/P. Kendra/indora-1549541559653.csv", sensor);        
         subory.put("src/main/resources/P. Kendra/indora-1549541572516.csv", sensor);        
         subory.put("src/main/resources/P. Kendra/indora-1549541582979.csv", sensor);        
         subory.put("src/main/resources/P. Kendra/indora-1549541633702.csv", sensor);        
@@ -164,9 +168,9 @@ public class LSTMNeuronka {
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541633702.csv", sensor);
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541647313.csv", sensor);        
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541673057.csv", sensor);        
-        subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541682687.csv", sensor); 
+        suboryNaTest.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541682687.csv", sensor); 
         
-        subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541695967.csv", sensor);
+        suboryNaTest.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541695967.csv", sensor);
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1549541708344.csv", sensor);
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553014481636.csv", sensor);        
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553014543983.csv", sensor);        
@@ -193,16 +197,24 @@ public class LSTMNeuronka {
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553610218773.csv", sensor);
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553610244950.csv", sensor);
         subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553610273270.csv", sensor);        
-        subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553610302262.csv", sensor);        
-        subory.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553610326669.csv", sensor); 
+        suboryNaTest.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553610302262.csv", sensor);        
+        suboryNaTest.put("src/main/resources/sk.upjs.indora.sensorsrecorder/indora-1553610326669.csv", sensor); 
         
-        // vygenerovanie datasetu 
+        // vygenerovanie treningoveho datasetu 
         for (Map.Entry<String, String> entry : subory.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
             System.out.println("Subor: " + key + " a akcelerometer: " + value);
-            processData(value,key);
+            processData(value,key,true);
         }    
+        
+        // vygenerovanie testovcieho datasetu 
+        for (Map.Entry<String, String> entry : suboryNaTest.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("Subor: " + key + " a akcelerometer: " + value);
+            processData(value,key,false);
+        }
         
         //LSTM neuronka odtial dalej
         // ----- Load the training data -----
@@ -210,8 +222,8 @@ public class LSTMNeuronka {
         //SequenceRecordReader trainFeatures = new CSVMultiSequenceRecordReader(csvSplitBy, CSVMultiSequenceRecordReader.Mode.EQUAL_LENGTH);
         SequenceRecordReader trainLabels = new CSVSequenceRecordReader();
         try {
-            trainFeatures.initialize(new NumberedFileInputSplit(featuresDirTrain.getAbsolutePath() + "/%d.csv", 0, filesCount-1));
-            trainLabels.initialize(new NumberedFileInputSplit(labelsDirTrain.getAbsolutePath() + "/%d.csv", 0, filesCount-1));
+            trainFeatures.initialize(new NumberedFileInputSplit(featuresDirTrain.getAbsolutePath() + "/%d.csv", 0, trainCount-1));
+            trainLabels.initialize(new NumberedFileInputSplit(labelsDirTrain.getAbsolutePath() + "/%d.csv", 0, trainCount-1));
         } catch (IOException ex) {
             java.util.logging.Logger.getLogger(LSTMNeuronka.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
@@ -231,19 +243,24 @@ public class LSTMNeuronka {
         //Use previously collected statistics to normalize on-the-fly. Each DataSet returned by 'trainData' iterator will be normalized
         trainData.setPreProcessor(normalizer);
 
-        /*
+        
         // ----- Load the test data -----
         //Same process as for the training data.
-        SequenceRecordReader testFeatures = new CSVSequenceRecordReader();
-        testFeatures.initialize(new NumberedFileInputSplit(featuresDirTest.getAbsolutePath() + "/%d.csv", 0, 149));
+        SequenceRecordReader testFeatures = new CSVSequenceRecordReader(0, csvSplitBy);
         SequenceRecordReader testLabels = new CSVSequenceRecordReader();
-        testLabels.initialize(new NumberedFileInputSplit(labelsDirTest.getAbsolutePath() + "/%d.csv", 0, 149));
-         
+        try {
+            testFeatures.initialize(new NumberedFileInputSplit(featuresDirTest.getAbsolutePath() + "/%d.csv", 0, testCount-1));        
+            testLabels.initialize(new NumberedFileInputSplit(labelsDirTest.getAbsolutePath() + "/%d.csv", 0, testCount-1));
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(LSTMNeuronka.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            java.util.logging.Logger.getLogger(LSTMNeuronka.class.getName()).log(Level.SEVERE, null, ex);
+        } 
         DataSetIterator testData = new SequenceRecordReaderDataSetIterator(testFeatures, testLabels, miniBatchSize, numLabelClasses,
             false, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END);
         
         testData.setPreProcessor(normalizer);   //Note that we are using the exact same normalization process as the training data
-        */
+        
 
         // ----- Configure the network -----
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
@@ -269,20 +286,21 @@ public class LSTMNeuronka {
         String str = "Test set evaluation at epoch %d: Accuracy = %.2f, F1 = %.2f";
         for (int i = 0; i < nEpochs; i++) {
             net.fit(trainData);
-/*
-            //Evaluate on the test set:
-            Evaluation evaluation = net.evaluate(testData);
-            log.info(String.format(str, i, evaluation.accuracy(), evaluation.f1()));
 
+            //Evaluate on the test set:
+            //Evaluation evaluation = net.evaluate(testData);            
+            //log.info(String.format(str, i, evaluation.accuracy(), evaluation.f1()));
+                       
             testData.reset();
-*/
             trainData.reset();
         }
+        Evaluation evaluation = net.evaluate(testData); 
+        log.info(evaluation.stats());
 
         log.info("----- Example Complete -----");
     }
     
-    public static void processData(String sensor, String csvFile){
+    public static void processData(String sensor, String csvFile, boolean forTraining){
         String line = "";
 	String cvsSplitBy = "\t";
         String currentActivity;
@@ -328,7 +346,7 @@ public class LSTMNeuronka {
                         } else {
                             //tu spracovat data aktivity a dat do suboru
                             System.out.println("pocet zaznamov aktivity " + currentActivity + "  :" + oneActivity.size());
-                            compute(oneActivity);                             
+                            compute(oneActivity, forTraining);                             
                             
                             oneActivity = new ArrayList<>();
                             break;
@@ -337,7 +355,7 @@ public class LSTMNeuronka {
                     //ak sme došli na koniec suboru a posledna aktivita vo file tak ju spracujeme
                     if (oneActivity.size() != 0) {
                         System.out.println("pocet zaznamov aktivity " + currentActivity + "  :" + oneActivity.size());
-                        compute(oneActivity); 
+                        compute(oneActivity, forTraining); 
                         
                         oneActivity = new ArrayList<>();
                     }
@@ -348,15 +366,23 @@ public class LSTMNeuronka {
         }
     }
     
-    private static void compute(List<double[]> oneActivity) {
+    private static void compute(List<double[]> oneActivity, boolean forTraining) {
         String features = "";
         for (double[] entry : oneActivity) {
             features = features + entry[1] + csvSplitBy + entry[2] + csvSplitBy + entry[3] + "\n";
         }
         
         //Write output in a format we can read, in the appropriate locations
-        File outPathFeatures = new File(featuresDirTrain, filesCount + ".csv");
-        File outPathLabels = new File(labelsDirTrain, filesCount + ".csv");
+        File outPathFeatures;
+        File outPathLabels;
+        if (forTraining) {
+            outPathFeatures = new File(featuresDirTrain, trainCount + ".csv");
+            outPathLabels = new File(labelsDirTrain, trainCount + ".csv");
+        } else {
+            outPathFeatures = new File(featuresDirTest, testCount + ".csv");
+            outPathLabels = new File(labelsDirTest, testCount + ".csv");
+        }
+        
        
         try {
             FileUtils.writeStringToFile(outPathFeatures, features);
@@ -364,6 +390,11 @@ public class LSTMNeuronka {
         } catch (IOException ex) {
             java.util.logging.Logger.getLogger(LSTMNeuronka.class.getName()).log(Level.SEVERE, null, ex);
         }
-        filesCount++;
+        
+        if (forTraining) {
+            trainCount++;
+        } else {
+            testCount++;
+        }        
     }  
 }
